f6ce96be915d55cbd5055ac162df370068e29cc7
Rogerio Pereira <hi@rogeriopereira.dev>
2023-06-13 20:39:41 -0400

    Injetando configMap na aplicação

--- index.php
+++ index.php
@@ -4,11 +4,28 @@ class Index
 {
     public function hello()
     {
+        $fileData = $this->getFileData("familyMembers/family.txt");
+
         /*
          * Using OS environment variables not variables defined in a .env file
          *      This variables are defines in deployment.yml
          */
-        return "<h1>Hello! I'm {$_ENV['NAME']}, i'm {$_ENV['AGE']} years old.</h1>";
+        return "<h1>
+                    Hello! I'm {$_ENV['NAME']}, i'm {$_ENV['AGE']} years old.    
+                </h1>
+                
+                <p>
+                    My family: {$fileData}
+                </p>";
+    }
+
+    private function getFileData($file)
+    {
+        try {
+            return file_get_contents($file);
+        } catch(Exception $e) {
+            exit("Error: File {$file} not found!");
+        }
     }
 }
 
